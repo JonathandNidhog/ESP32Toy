cdefba8e1faef5e7bd891bb8ea99d92330e13bd7
Daiyu Wei <65467723+JonathandNidhog@users.noreply.github.com>
2026-05-21 03:13:18 +0800

    Boot DoomGeneric from LittleFS doom1 wad and idle on missing file

--- Arduino/ESP32Toy_DoomGeneric_Phase3/Phase3_entry.ino
+++ Arduino/ESP32Toy_DoomGeneric_Phase3/Phase3_entry.ino
@@ -1,14 +1,32 @@
 #include <Arduino.h>
 #include "doomgeneric_esp32toy.h"
 
+static bool doomStarted = false;
+
 void setup() {
   ESP32Toy_DoomPlatformInitHardware();
+
+  if (!ESP32Toy_DoomPlatformHasIWAD()) {
+    doomStarted = false;
+    return;
+  }
+
   static char arg0[] = "esp32toy";
-  static char *argv[] = { arg0 };
-  doomgeneric_Create(1, argv);
+  static char arg1[] = "-iwad";
+  static char arg2[] = "/littlefs/doom1.wad";
+  static char *argv[] = { arg0, arg1, arg2 };
+
+  doomgeneric_Create(3, argv);
+  doomStarted = true;
 }
 
 void loop() {
+  if (!doomStarted) {
+    ESP32Toy_DoomPlatformIdle();
+    delay(30);
+    return;
+  }
+
   ESP32Toy_DoomPlatformBeforeTick();
   doomgeneric_Tick();
   ESP32Toy_DoomPlatformAfterTick();
