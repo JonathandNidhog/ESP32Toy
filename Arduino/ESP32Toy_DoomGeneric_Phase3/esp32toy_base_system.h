4b842219b7c76ef05f39f4ba96eea3d268b9cb83
Daiyu Wei <65467723+JonathandNidhog@users.noreply.github.com>
2026-05-22 01:07:55 +0800

    Remove Water Lab macro redirect

--- Arduino/ESP32Toy_DoomGeneric_Phase3/esp32toy_base_system.h
+++ Arduino/ESP32Toy_DoomGeneric_Phase3/esp32toy_base_system.h
@@ -2,7 +2,6 @@
 
 #include <Arduino.h>
 
-// Base-system launcher actions returned by ESP32Toy_OSLauncherTick().
 enum ESP32Toy_OSAction : uint8_t {
   ESP32TOY_OS_ACTION_NONE = 0,
   ESP32TOY_OS_ACTION_START_WATER = 1,
@@ -12,9 +11,5 @@ enum ESP32Toy_OSAction : uint8_t {
 void ESP32Toy_OSInitLauncher(bool doomReady);
 ESP32Toy_OSAction ESP32Toy_OSLauncherTick(bool doomReady);
 void ESP32Toy_OSRedrawLauncher(bool doomReady);
-
-// Water Lab V2 is a standalone LiquidOS-style module.  The old implementation
-// remains in esp32toy_base_system.cpp, but the application entry is redirected
-// here so Doom/base launcher code does not need to be touched.
+bool ESP32Toy_OSWaterTick(void);
 bool ESP32Toy_WaterLabV2Tick(void);
-#define ESP32Toy_OSWaterTick ESP32Toy_WaterLabV2Tick
